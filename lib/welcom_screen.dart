da23e58365db0b3415acc188df5349a4894b9702
habibTeck <abdualhabibabdualrahman@gmail.com>
2022-11-02 02:59:12 +0300

    "edit to welcome"

--- lib/welcom_screen.dart
+++ lib/welcom_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+// new Edit to Welcome Screen
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
